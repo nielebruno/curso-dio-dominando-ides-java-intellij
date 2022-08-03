b1af4c76fb7014147878f7a1d01bb5736f761a9d
Daniele Bruno <nielebruno@gmail.com>
2022-08-03 00:51:12 -0300

    First commit

---
+++ primeiro-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro sherlock = new Cachorro();
+        System.out.println(sherlock);
+    /*    int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a+b));*/
+
+    }
+}
